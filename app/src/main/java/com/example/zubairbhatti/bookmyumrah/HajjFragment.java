e3810bb70b26216a780fde26ccf2ef7baa82a771
Khurram Mir <khurram.mir@bridgevine.com>
2016-04-07 13:10:33 +0500

    Test Message from Khurram - Testing Purposes

--- app/src/main/java/com/example/zubairbhatti/bookmyumrah/HajjFragment.java
+++ app/src/main/java/com/example/zubairbhatti/bookmyumrah/HajjFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
+// This comment is added by Khurram for testing purposes
 /**
  * A simple {@link Fragment} subclass.
  */
